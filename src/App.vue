253403ece296d115c86a10b0063fe47bc42e1274
MollyCoddle Tech <spoonfeedertech@gmail.com>
2020-07-02 14:53:27 +0100

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to my page"/>
+    <HelloWorld msg="Welcome Hp to my page"/>
   </div>
 </template>
 
